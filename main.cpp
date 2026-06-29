5e52c796f3276c72234d657efe345bd0c9d5687d
j-brisard <julesbrisard@gmail.com>
2026-06-29 17:57:13 +0200

    Add a hollow glass sphere to the scene

--- main.cpp
+++ main.cpp
@@ -16,11 +16,14 @@ int main() {
     auto material_left   = make_shared<metal>(color(0.2, 0.2, 0.8),0.2);
     auto material_center = make_shared<lambertian>(color(0.8, 0.8, 0.8));
     auto material_right  = make_shared<dielectric>(1.5);//1.5/1 is the refraction index of glass inside of air
+    auto material_bubble  = make_shared<dielectric>(1.0/1.5);//1.0/1.5 is the refraction index of air inside of glass
+
     //Spheres
     world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
     world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
     world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.2),   0.5, material_center));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));//Glass sphere
+    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.4, material_bubble));//Adds an air sphere inside the glass sphere
     
     //Planes
     //world.add(make_shared<plane>(point3(-3.0,    0.0, -1.0), vec3(1,,0), material_right));
