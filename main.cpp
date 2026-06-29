4816380252da7ea1c5d65e6d3ac1bbab595903d4
j-brisard <julesbrisard@gmail.com>
2026-06-29 18:34:26 +0200

    Add camera position and orientation with lookfrom and lookat points

--- main.cpp
+++ main.cpp
@@ -11,10 +11,9 @@ int main() {
 
     hittable_list world;
 
-    /*
     //Materials
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_left   = make_shared<metal>(color(0.2, 0.2, 0.8),0.2);
+    auto material_left   = make_shared<metal>(color(0.2, 0.2, 0.8),0);
     auto material_center = make_shared<lambertian>(color(0.8, 0.8, 0.8));
     auto material_right  = make_shared<dielectric>(1.5);//1.5/1 is the refraction index of glass inside of air
     auto material_bubble  = make_shared<dielectric>(1.0/1.5);//1.0/1.5 is the refraction index of air inside of glass
@@ -29,24 +28,18 @@ int main() {
     //Planes
     //world.add(make_shared<plane>(point3(-3.0,    0.0, -1.0), vec3(1,,0), material_right));
     //world.add(make_shared<plane>(point3( 3.0,    0.0, -1.0), vec3(-1,0,0), material_left));
-    */
-
-    auto R = std::cos(pi/4);
-
-    auto material_left  = make_shared<lambertian>(color(0,0,1));
-    auto material_right = make_shared<lambertian>(color(1,0,0));
-
-    world.add(make_shared<sphere>(point3(-R, 0, -1), R, material_left));
-    world.add(make_shared<sphere>(point3( R, 0, -1), R, material_right));
 
     camera cam;
 
     cam.aspect_ratio = 16.0/9.0;
-    cam.image_width = 400;
+    cam.image_width = 600;
     cam.samples_per_pixel = 100;
     cam.max_depth = 100;
 
-    cam.vfov = 20;
+    cam.vfov = 40;
+    cam.lookfrom = point3(-1,1,1);
+    cam.lookat   = point3(0,0,-1);
+    cam.vup      = vec3(0,1,0);
     
     cam.render(world);
 }
